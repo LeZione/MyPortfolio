d1090f7b3039d973058b6cc00d62c6bb2c0adbf5
LeZione <eprado706@gmail.com>
2024-08-04 21:48:53 +0800

    quick fix to myProfile

--- public/pages/myProfile.php
+++ public/pages/myProfile.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang = "en">
     <head>
-        <link rel="stylesheet" href="/../public/pages/myProfile.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Keith's Profile</title>
+        <link rel="stylesheet" href="/myProfile.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Familjen+Grotesk&family=Farro&family=Gotu&family=Licorice&family=Livvic:wght@100&family=Lobster&family=M+PLUS+Code+Latin&family=Source+Code+Pro&display=swap" rel="stylesheet">
