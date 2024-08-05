3b122f85ceb1035a2e79a25d69a9708521cf3a5c
LeZione <eprado706@gmail.com>
2024-08-05 09:17:56 +0800

    Profile Picture and brief intro

--- public/pages/myProfile.php
+++ public/pages/myProfile.php
@@ -4,10 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Keith's Profile</title>
-        <link rel="stylesheet" href="/style.css"> 
+        <link rel="stylesheet" href="style.css"> 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Familjen+Grotesk&family=Farro&family=Gotu&family=Licorice&family=Livvic:wght@100&family=Lobster&family=M+PLUS+Code+Latin&family=Source+Code+Pro&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="reset.css">
     </head>
 
@@ -28,13 +29,12 @@
             <div class="mainContent">
                 <div class="myIntroduction">
                     <div class="myPicture">
-                        <!-- Add an image here if needed -->
+                        <img src = "img/Pradoo.png"> 
                     </div>
                     <div class="introSentence">
-                        <h1>I am</h1>
-                        <h1>Keith</h1>
-                        <h3>Bachelor of Engineering Technology</h3>
-                        <h3>Major in Computer Engineering Technology</h3>
+                        <div class ="a">Hi, I am Keith!</div>
+                        <div class = "b">Bachelor of Engineering Technology</div>
+                        <div class = "c">major in Computer Engineering Technology</div>
                     </div>
                 </div>
             </div>
