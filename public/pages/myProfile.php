9a7ee81e560078863af75fafecd1f60ae06c8062
LeZione <eprado706@gmail.com>
2024-08-08 07:39:51 +0800

    Picture and Name's Design and Layout

--- public/pages/myProfile.php
+++ public/pages/myProfile.php
@@ -11,7 +11,6 @@
         <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="reset.css">
     </head>
-
     <body>
         <div class="sideNav">
             <div class="menu">
@@ -29,12 +28,14 @@
             <div class="mainContent">
                 <div class="myIntroduction">
                     <div class="myPicture">
-                        <img src = "/assets/img/Pradoo.png"> 
+                        <img src = "img/Pradoo.png"> 
                     </div>
                     <div class="introSentence">
-                        <div class ="a">Hi, I am Keith!</div>
-                        <div class = "b">Bachelor of Engineering Technology</div>
-                        <div class = "c">major in Computer Engineering Technology</div>
+                        <div class = "myNameAndCourse">
+                            <div class ="a">Hi, I am Keith!</div>
+                            <div class = "b">Bachelor of Engineering Technology</div>
+                            <div class = "c">major in Computer Engineering Technology</div>
+                        </div>
                     </div>
                 </div>
             </div>
