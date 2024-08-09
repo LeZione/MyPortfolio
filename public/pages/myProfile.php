521e51f9b75e6b937c6b11cd60a10641ac23cf13
LeZione <eprado706@gmail.com>
2024-08-09 10:29:02 +0800

    Navigation Menu

--- public/pages/myProfile.php
+++ public/pages/myProfile.php
@@ -9,6 +9,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Familjen+Grotesk&family=Farro&family=Gotu&family=Licorice&family=Livvic:wght@100&family=Lobster&family=M+PLUS+Code+Latin&family=Source+Code+Pro&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link rel="stylesheet" href="reset.css">
     </head>
     <body>
@@ -34,12 +35,12 @@
 
                 <div class="stickyNav">
                     <div class="menu">
-                        <a href="#">About Me</a>
-                        <a href="#">Skills</a>
-                        <a href="#">Experience</a>
-                        <a href="#">Awards and Achievements</a>
-                        <a href="#">Training and Seminars</a>
-                        <a href="#">Contact Me</a>
+                        <a href="#" class="icon-about"><span>About Me</span></a>
+                        <a href="#" class="icon-skills"><span>Skills</span></a>
+                        <a href="#" class="icon-experience"><span>Experience</span></a>
+                        <a href="#" class="icon-awards"><span>Awards and Achievements</span></a>
+                        <a href="#" class="icon-training"><span>Training and Seminars</span></a>
+                        <a href="#" class="icon-contact"><span>Contact Me</span></a>
                     </div>
                 </div>
             </div>
